2887aed1850b2e021a47c36fa980790f23431ed4
ipastorl <isabel.pastorlopez@emeal.nttdata.com>
2024-02-27 17:02:45 +0100

    Validaciones y paginación

--- src/main/java/com/nttdata/microservicios/app/examenes/controllers/ExamenController.java
+++ src/main/java/com/nttdata/microservicios/app/examenes/controllers/ExamenController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -14,10 +15,15 @@ import com.nttdata.microservicios.app.examenes.services.ExamenService;
 import com.nttdata.microservicios.commons.controllers.CommonController;
 import com.nttdata.microservicios.commons.examenes.models.entity.Examen;
 
+import jakarta.validation.Valid;
+
 @RestController
 public class ExamenController extends CommonController<Examen, ExamenService> {
 	@PutMapping("/{id}")
-	public ResponseEntity<?> editar(@RequestBody Examen examen, @PathVariable Long id){
+	public ResponseEntity<?> editar(@Valid @RequestBody Examen examen, BindingResult result, @PathVariable Long id){
+		if(result.hasErrors()) {
+			return this.validar(result);
+		}
 		Optional<Examen> o = service.findById(id);
 		if(!o.isPresent()) {
 			return ResponseEntity.notFound().build();
--- src/main/java/com/nttdata/microservicios/app/examenes/models/repository/ExamenRepository.java
+++ src/main/java/com/nttdata/microservicios/app/examenes/models/repository/ExamenRepository.java
@@ -2,12 +2,12 @@ package com.nttdata.microservicios.app.examenes.models.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 
 import com.nttdata.microservicios.commons.examenes.models.entity.Examen;
 
-public interface ExamenRepository extends CrudRepository<Examen, Long> {
+public interface ExamenRepository extends JpaRepository<Examen, Long> {
 	
 	@Query("select e from Examen e where e.nombre like %?1%")
 	public List<Examen> findByNombre(String term);
